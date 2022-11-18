850610bee10126d5b2d3ef341c2e6bf8af40e53d
Sophie Watts <sophiewatts@Sophies-MacBook-Pro-6.local>
2022-11-18 12:23:12 -0800

    remove irrelevant files

--- shell_scripts/background_ld.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:30:00
-#SBATCH --mem=64G
-#SBATCH --output=background_ld.out
-#SBATCH --job-name=background_ld
-
-Rscript background_ld.R
--- shell_scripts/ld_decay_curve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --time=3-00:00:00
-#SBATCH --mem=64G
-#SBATCH --output=ld_decay_curve.out
-#SBATCH --job-name=ld_decay_curve
-
-Rscript ld_decay_curve.R
